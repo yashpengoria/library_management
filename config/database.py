d17e70a463ea6582a8c10158796f97050bcc3dce
yashpengoria <yashpengoria2001@gmail.com>
2024-04-09 19:16:10 +0530

    final commit

--- config/database.py
+++ config/database.py
@@ -1,10 +1,19 @@
 from pymongo import MongoClient
 import os
 from dotenv import load_dotenv
+import urllib.parse
 
 load_dotenv()
 
-MONGODBURL=os.getenv("MONGODBURL")
+encoded_username = urllib.parse.quote_plus(os.getenv("username"))
+encoded_password = urllib.parse.quote_plus(os.getenv("password"))
+
+# Construct the MongoDB URI with the encoded username and password
+
+
+MONGODBURL = f"mongodb+srv://{encoded_username}:{encoded_password}@cluster0.skqct13.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
+# MONGODBURL=os.getenv("MONGODBURL")
 
 client=MongoClient(MONGODBURL)
 
